f34334972f0ab0ad8ac094bac28827d75d8fb09c
rasha <82375859+rasha-taher@users.noreply.github.com>
2024-01-25 17:03:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const PORT = 8080;
 app.use(express.json());
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
