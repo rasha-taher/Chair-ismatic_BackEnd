a2233d22ed68d9c7a4fe5e3c19c3ccd90b630186
rasha <82375859+rasha-taher@users.noreply.github.com>
2024-01-30 11:02:03 +0200

    Update ConversationController.js

--- controllers/ConversationController.js
+++ controllers/ConversationController.js
@@ -10,7 +10,7 @@ const newConversation = async (req, res) => {
     const savedConversation = await newConversation.save();
     res.status(200).json(savedConversation);
   } catch (err) {
-    res.stautus(500).json(err);
+    res.status(500).json(err);
   }
 };
 
